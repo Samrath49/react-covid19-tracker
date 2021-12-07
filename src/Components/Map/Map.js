43d9184ec330fb0485785cd6455e5260104d66b8
Samrath Chauhan <55148801+Samrath49@users.noreply.github.com>
2021-12-07 19:39:33 +0530

    App changes

--- src/Components/Map/Map.js
+++ src/Components/Map/Map.js
@@ -1,17 +1,20 @@
-import React from 'react';
+import React from "react";
 import { Map as LeafletMap, TileLayer } from "react-leaflet";
-import './Map.css';
+import "./Map.css";
+import {showDataOnMap} from '../../utility/util';
 
-function Map({center, zoom}) {
-    return (
-        <div claassName="map">
-            <h1>I am a map</h1>
-            <LeafletMap center={center} zoom={zoom}>
-                <TileLayer url="https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png"
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' />
-            </LeafletMap>
-        </div>
-    )
+function Map({ countries, casesType, center, zoom }) {
+  return (
+    <div className="map">
+      <LeafletMap center={center} zoom={zoom}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        {showDataOnMap(countries, casesType)}
+      </LeafletMap>
+    </div>
+  );
 }
 
-export default Map
+export default Map;
